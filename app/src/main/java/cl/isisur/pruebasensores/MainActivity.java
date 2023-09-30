d8d023ac85f34de133da3a8753238793a31a58be
Pablo Calderon <pablocalderoncornejo@gmail.com>
2023-09-30 12:52:05 -0300

    V6

--- app/src/main/java/cl/isisur/pruebasensores/MainActivity.java
+++ app/src/main/java/cl/isisur/pruebasensores/MainActivity.java
@@ -2,20 +2,28 @@ package cl.isisur.pruebasensores;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+
+import android.content.Context;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
-
+    private SensorManager mgr;
+    private TextView textLista;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
         mgr=(SensorManager) getSystemService(Context.SENSOR_SERVICE);
         textLista =(TextView) findViewById(R.id.sensorsList);
         List<Sensor> sensorList=mgr.getSensorList(Sensor.TYPE_ALL);
         StringBuilder stringBuilder= new StringBuilder();
         for (Sensor s: sensorList){
-            stringBuilder.append(s.getName()).append("\n");
+            stringBuilder.append(s.getName());
         }
 
         textLista.setVisibility(View.VISIBLE);
